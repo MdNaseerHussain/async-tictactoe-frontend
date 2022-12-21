7b21738c0a2933b2f8c2ef68cb4320f74d6720b1
MdNaseerHussain <mdnajju013@gmail.com>
2022-12-21 15:07:50 +0530

    Board styling done

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,20 +1,29 @@
 import "./Board.css";
+import BlankLogo from "../BlankLogo.svg";
+import Ologo from "../Ologo.svg";
+import Xlogo from "../Xlogo.svg";
 
 function Board() {
   return (
     <table id="board">
       <tr>
+        <td>
+          <img src={Xlogo} alt="X" />
+        </td>
+        <td>
+          <img src={Ologo} alt="O" />
+        </td>
         <td></td>
+      </tr>
+      <tr>
+        <td>
+          <img src={BlankLogo} alt="" />
+        </td>
         <td></td>
         <td></td>
       </tr>
       <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-      </tr>
-      <tr>
-        <td></td>
+        <td><img src={BlankLogo} alt="" /></td>
         <td></td>
         <td></td>
       </tr>
--- src/pages/Play.js
+++ src/pages/Play.js
@@ -1,9 +1,30 @@
-import Board from "../components/Board"
+import { useNavigate } from "react-router-dom";
+import Board from "../components/Board";
+import Button from "../components/Button";
+import "../index.css";
+import BackArrow from "../arrow_back_ios.svg";
+import Xlogo from "../Xlogo.svg";
 
 function Play() {
+  const navigate = useNavigate();
   return (
-    <div><Board /></div>
-  )
+    <div>
+      <img
+        src={BackArrow}
+        alt="Back"
+        className="back-arrow"
+        onClick={() => navigate('/games')}
+      />
+      <p className="title title-game">Game with Naseer</p>
+      <p className="piece">Your Piece</p>
+      <img src={Xlogo} alt="X" className="piece-logo" />
+      <p className="board-label">Your Move</p>
+      <Board />
+      <div className="btn-wrapper">
+        <Button text="Submit" styling="btn" />
+      </div>
+    </div>
+  );
 }
 
-export default Play
+export default Play;
